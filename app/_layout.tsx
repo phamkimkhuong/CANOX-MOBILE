29089021a06833965b6af9f36a803d4c41e14d1d
phamkimkhuong <phamkhuong436@gmail.com>
2026-01-14 09:31:38 +0700

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -34,6 +34,7 @@ import Toast from 'react-native-toast-message';
 // Configure Google Sign-In
 GoogleSignin.configure({
   webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+  iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
   offlineAccess: true,
 });
 
