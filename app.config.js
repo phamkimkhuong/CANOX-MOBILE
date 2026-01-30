ffe7d377a419b3717ef9c690d33db8a70d546843
phamkimkhuong <phamkhuong436@gmail.com>
2026-01-30 08:11:25 +0700

    feat: Add Expo plugins for Android notification icon/color customization and 16KB page size support.

--- app.config.js
+++ app.config.js
@@ -38,6 +38,7 @@ module.exports = {
         },
 
         "plugins": [
+            "./plugins/withAndroid16KB",
             "./plugins/withNotifee",
             [
                 "./plugins/withNotificationIcon",
